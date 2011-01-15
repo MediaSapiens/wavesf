1caba034f7ed1af96f30c932800d017230565420
Sergej <sfelde@gmail.com>
2011-01-15 16:57:16 +0100

    fix ursls

--- urls.py
+++ urls.py
@@ -9,9 +9,9 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     #('^$', 'django.views.generic.simple.direct_to_template',{'template': 'home.html'}),
-	('^$', include('gadget.urls')),
+	('^', include('gadget.urls')),
     (r'^admin/', include(admin.site.urls)),
-	(r'^gadget/', include('gadget.urls')),
+	#(r'^gadget/', include('gadget.urls')),
 	(r'^accounts/', include('registration.backends.default.urls')),
 	(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
 )
